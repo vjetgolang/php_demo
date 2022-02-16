450cdd2921df84ead2b2d87d0c100ad0d4bc8b94
Sang Hồ <sangho@Sangs-MacBook-Air.local>
2022-02-16 23:15:19 +0700

    submit

---
+++ sang/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    echo"";
+    
+    echo" form- html
+    loop 1-20 ";
+    echo"<br>";
+    $_count=20;
+    for($i=0;$i<=$_count;$i++){
+        echo" $i <br>";
+    };
+    ?>
+</body>
+</html>
