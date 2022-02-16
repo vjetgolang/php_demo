fd18bf27812e0bfba32646384cdc0938a4f75555
Diii <hoduy244@gmail.com>
2022-02-16 22:37:26 +0700

    bt

---
+++ dii/form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="print.php">
+        ho va ten
+        <input type="text" name="name">
+        <br>
+        nam sinh
+        <input type="date" name="ngaysinh">
+        <br>
+        mail
+        <input type="email" name="mail" id="">
+        <br>
+        nhap mat khau
+        <input type="password" name="password" id="">
+        <br>
+        <button>Hoan thanh</button>
+        <br>
+        <input type="submit" value = "OK">
+    </form>
+</body>
+</html>
---
+++ dii/print.php
@@ -0,0 +1,6 @@
+<?php
+$hovaten=$_GET['name'];
+$namsinh=$_GET['ngaysinh'];
+$mail=$_GET['mail'];
+$matkhau=$_GET['password'];
+?>
