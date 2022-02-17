510b6744f9169fe782eba6e5718dcdee9e543a7e
Diii <hoduy244@gmail.com>
2022-02-17 10:20:44 +0700

    updateD

---
+++ duy/printInfo.php
@@ -0,0 +1,12 @@
+<?php 
+    $ten = $_GET['ten'];
+    $ngaysinh = $_GET['ngaysinh'];
+    $gioitinh = $_GET['sex'];
+    $email = $_GET['email'];
+    $password = $_GET['password'];
+    echo "Tên: $ten <br>"; 
+    echo "Ngày sinh: $ngaysinh <br>";
+    echo "Giới tính: $gioitinh <br>";
+    echo "Email: $email <br>";
+    echo "Password: $password <br>";
+?>
