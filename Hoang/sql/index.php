d27b57f1b697c5375f981e506060563e2028ff2c
011223345aA <huyhoang11202@gmail.com>
2022-02-20 19:23:19 +0700

    sql

---
+++ Hoang/sql/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   <form action="conect_sql.php" method="post">
+      Nhap mssv:
+      <input type="text" name="mssv"><br>
+      Nhap Ho Ten:
+      <input type="text" name="ten"><br>
+      Nhap Lop:
+      <input type="text" name="lop">
+      <button>Send</button>
+   </form>
+</body>
+</html>
